8e37ff730c0ca9211f50744077f7762d5f7ddd10
YariK <xudeku92@gmail.com>
2022-05-28 17:21:38 +0300

    add Seminar 9

---
+++ SEMINARS/Seminar9/Program.cs
@@ -0,0 +1,30 @@
+﻿//Пользователь вводит чсило n, написать рекурсивный метод который выведит все натуральные число от 1 до n.
+/*
+NaturalNums(5);
+
+void NaturalNums(int n)
+{
+    if (n >= 1)
+    {
+        NaturalNums(n - 1);
+        Console.Write(n + " ");
+    }
+}
+*/
+
+// n^m = n * n * n * n.....
+
+
+
+int Recurs(int n, int m)
+{
+    if (m >= 1)
+    {
+        return n * Recurs(n, m - 1);
+
+    }
+    else
+        return 1;
+}
+
+Console.WriteLine(Recurs(5, 4));
